5f9e03180e804026003bd9ce1eb9bcf41d19065f
Mikhail Volkov <47795110+mikhail-vl@users.noreply.github.com>
2023-06-12 07:32:37 +0100

    Add device configuration for Config transformation (#43)

--- timescale/server.ts
+++ timescale/server.ts
@@ -17,16 +17,16 @@ const addMetrics = async () => {
   temp += Math.random();
   temp2 += Math.random();
 
-  await client.query(`insert into metrics values(nextval('seq_metrics'), now(), 'test', ${temp});`);
-  await client.query(`insert into metrics values(nextval('seq_metrics'), now(), 'test2', ${temp2});`);
+  await client.query(`insert into metrics values(nextval('seq_metrics'), now(), 'device1', ${temp});`);
+  await client.query(`insert into metrics values(nextval('seq_metrics'), now(), 'device2', ${temp2});`);
 
   let timeout = 1000;
   if (temp > 100 || temp2 > 100) {
     temp = 20;
     temp2 = 20;
 
-    await client.query(`insert into controls values(nextval('seq_controls'), now(), 'test', ${temp});`);
-    await client.query(`insert into controls values(nextval('seq_controls'), now(), 'test2', ${temp2});`);
+    await client.query(`insert into controls values(nextval('seq_controls'), now(), 'device1', ${temp});`);
+    await client.query(`insert into controls values(nextval('seq_controls'), now(), 'device2', ${temp2});`);
 
     timeout = 5000;
   }
